1b67276fc759e7ac26326a721dfd468ebbda7066
Umar Fadhlurrachman <umar.fadhlurrachman@berca.co.id>
2026-02-25 22:55:57 +0700

    Check html

--- palmsugar/resources/views/components/hero.blade.php
+++ palmsugar/resources/views/components/hero.blade.php
@@ -6,7 +6,6 @@
         @foreach ($sliders as $key => $slide)
             <div class="carousel-item {{ $key == 0 ? 'active' : '' }}">
 
-                <!-- Gambar Slider (Pastikan path 'storage' bisa diakses browser) -->
                 <img src="{{ asset('storage/' . $slide->image) }}" alt="{{ $slide->headline }}">
 
                 <div class="carousel-container">
@@ -16,12 +15,13 @@
                         <p>{{ $slide->subheadline }}</p>
                     @endif
 
-                    <!-- Tombol CTA (Opsional) -->
                     @if ($slide->cta_text && $slide->cta_url)
-                        <a href="{{ $slide->cta_url }}"
-                            class="btn-get-started animate__animated animate__fadeInUp">{{ $slide->cta_text }}</a>
+                        <a href="{{ $slide->cta_url }}" class="btn-get-started animate__animated animate__fadeInUp">
+                            {{ $slide->cta_text }}
+                        </a>
                     @endif
                 </div>
+
             </div>
         @endforeach
 
@@ -35,13 +35,8 @@
             <span class="carousel-control-next-icon bi bi-chevron-right" aria-hidden="true"></span>
         </a>
 
-        <!-- Indikator Bulat-bulat di Bawah -->
-        <ol class="carousel-indicators">
-            @foreach ($sliders as $key => $slide)
-                <li data-bs-target="#hero-carousel" data-bs-slide-to="{{ $key }}"
-                    class="{{ $key == 0 ? 'active' : '' }}"></li>
-            @endforeach
-        </ol>
+        <!-- Kosongkan, biar JS hitung otomatis dari jumlah .carousel-item -->
+        <ol class="carousel-indicators"></ol>
 
     </div>
 
